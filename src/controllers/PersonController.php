4245ea8f598a8d21af36808983313742ce0605fe
JorgeReveron <jorgealexanderclases@gmail.com>
2023-11-21 20:17:10 +0000

    Person controller CR-D

---
+++ src/controllers/PersonController.php
@@ -0,0 +1,39 @@
+<?php
+namespace Jorge\People\Controllers;
+use Jorge\People\Models\Person;
+
+class PersonController {
+
+  public function list() {
+    $persons = Person::all();
+    require("../src/views/person/list.php");
+  }
+
+  public function show($id) {
+    $person = Person::find($id);
+    if ($person) {
+      require("../src/views/person/show.php");
+    }else {
+      $this->list();
+    }
+  }
+
+  public function delete($id) {
+    $person = Person::find($id);
+    if($person) {
+      $person->delete();
+    }
+    $this->list();
+  }
+
+  public function create() {
+    require("../src/views/person/create.php");
+  }
+
+  public function post($data) {
+    $person = new Person();
+    $person->name = $data["name"];
+    $person->save();
+    $this->list();
+  }
+}
---
+++ src/models/Person.php
@@ -0,0 +1,8 @@
+<?php
+namespace Jorge\People\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model {
+  // protected $table = "Persons";
+}
